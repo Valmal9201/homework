920d7ddb6f262f6793b543809c7f0e2037241959
Valmal9201 <zeng.cynthia236@gmail.com>
2026-02-17 11:45:46 -0500

    Square Problem Solution

--- lesson03/lesson3.py
+++ lesson03/lesson3.py
@@ -1,1 +1,6 @@
 # Lesson 3
+num = int(input("Enter a positive integer: "))
+size = int((num ** 0.5))
+
+print(f"The largest square made with {num} is:")
+print(f"Side length: {size} tiles\nArea: {size * size} tiles squared")
