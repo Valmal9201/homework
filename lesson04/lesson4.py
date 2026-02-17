968858c4ac764af5a65f0915b588045e07fd2ace
Valmal9201 <zeng.cynthia236@gmail.com>
2026-02-17 17:40:47 -0500

    Fence Problem Solution

--- lesson04/lesson4.py
+++ lesson04/lesson4.py
@@ -1,1 +1,11 @@
 # Lesson 4
+import math
+count = 0
+fence  = 0
+while (count < 3):
+    fence += len((input("Enter the length of the fence in F's: ex (3 = FFF): ")))
+    count += 1
+
+cans = math.ceil(fence/12)
+print (f"{fence}\n{(cans * 12 - fence) % 12}\n{cans * 14.90}")
+
