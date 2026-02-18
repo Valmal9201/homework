94fa4cd1a014ae3b87cf04a595397e4b7ca126c9
Valmal9201 <zeng.cynthia236@gmail.com>
2026-02-18 15:37:18 -0500

    Telemarketer Problem Solution

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,8 @@
 # Lesson 10
+num = input("Enter the last 4 digits of the number: ")
+if num[0] == "8" or num[0] == "9":
+    if num[-1] == "8" or num[-1] == "9":
+        if num[1] == num[2]:
+            print("Telemarketer")
+else:
+    print("Not a telemarketer")
