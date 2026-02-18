b19b20d55a821c1b4dd049ba46408737c3baa0e6
Valmal9201 <zeng.cynthia236@gmail.com>
2026-02-18 10:15:58 -0500

    Cookie Problem Solution

--- lesson05/lesson5.py
+++ lesson05/lesson5.py
@@ -1,1 +1,18 @@
 # Lesson 5
+money = float(input("Enter starting money: $"))
+cookie = input("Enter cookies as letters (b = big, c = small): ")
+small = 0
+big = 0
+profit = 0
+totalCookies = cookie.count("c") + cookie.count("b")
+
+for i in cookie:
+    if i == "c":
+        small += 1
+        profit += 0.75
+    elif i == "b":
+        big += 1
+        profit += 1.25
+    else:
+        print(f"{i} is not a valid item.")
+print(f"Cookies sold: {totalCookies}\nProfit: ${profit}\nFinal Amount: ${profit + money}")
