6376d835d1311a1c0a9a99c7932da452eaf87baa
Valmal9201 <zeng.cynthia236@gmail.com>
2026-02-18 10:32:41 -0500

    D20 Problem Solution

--- lesson07/lesson7.py
+++ lesson07/lesson7.py
@@ -1,1 +1,10 @@
 # Lesson 7
+from random import randrange
+dc = int(input("Enter the DC: "))
+ran = randrange(1, 21)
+
+print(f"D20 roll: {ran}")
+if (ran < dc):
+    print("Failed Check")
+else:
+    print ("Passed Check")
