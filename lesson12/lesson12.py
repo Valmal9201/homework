356bce13ea73f333a9ed530270fa248f9c3c576e
Valmal9201 <zeng.cynthia236@gmail.com>
2026-02-20 11:19:00 -0500

    Triangle Problem Solution

--- lesson12/lesson12.py
+++ lesson12/lesson12.py
@@ -1,1 +1,14 @@
 # Lesson 12
+a = int(input("Enter Angle A: "))
+b = int(input("Enter Angle B: "))
+c = int(input("Enter Angle C: "))
+
+if a + b + c == 180:
+    if a == b == c == 60:
+        print("Equilateral Triangle")
+    elif a != b != c:
+        print("Scalene Triangle")
+    else:
+        print("Isosceles Triangle")
+else:
+    print("Not Triangle")
