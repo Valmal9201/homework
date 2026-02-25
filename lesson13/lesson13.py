4bc00b456d4286ac4219b5063108aaade043340b
Valmal9201 <zeng.cynthia236@gmail.com>
2026-02-24 20:10:31 -0500

    February Problem Solution

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,16 @@
 # Lesson 13
+month = int(input("Enter month as number (Ex. 1 = Jan, 12 = Dec): "))
+day = int(input("Enter day as number: "))
+
+if month > 0 and month < 13 and day > 0 and day < 32:
+    if month <= 2:
+        if (month == 2 and day < 18) or month == 1:
+            print("Before")
+        elif month == 2 and day == 18:
+            print("Special")
+        elif month == 2 and (19 <= day <= 29):
+            print("After")  
+    elif month >= 3 and month <= 12:
+        print("After")
+else:
+    print("Not a date.")
