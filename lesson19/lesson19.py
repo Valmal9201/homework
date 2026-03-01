6fcfce2636e5e7dc01d9b03d7c2a5eac0b2143e2
Valmal9201 <zeng.cynthia236@gmail.com>
2026-03-01 12:09:24 -0500

    Prime Number Problem 2 Solutions

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,33 @@
 # Lesson 19
+# 1
+num = int(input())
+i = 2
+ctr = 0
+
+while i < num:
+    if num % i == 0:
+        ctr += 1
+        break
+    i += 1
+
+if ctr > 0:
+    print("Composite")
+else:
+    print("Prime")
+
+# 2
+num = int(input())
+i = 2
+stop = num ** 0.5
+ctr = 0
+
+while i < stop:
+    if num % i == 0:
+        ctr += 1
+        break
+    i += 1
+
+if ctr > 0:
+    print("Composite")
+else:
+    print("Prime")
