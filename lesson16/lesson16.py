205eb3d629be75a5169c4a1b6e11a3dc24746b6b
Valmal9201 <zeng.cynthia236@gmail.com>
2026-03-01 10:45:00 -0500

    Fizzbuzz Problem Solution

--- lesson16/lesson16.py
+++ lesson16/lesson16.py
@@ -1,1 +1,9 @@
 # Lesson 16
+num = range(1, 51)
+for i in num:
+    if i % 5 == 0 and i % 3 == 0:
+        print(f"{i}: Fizzbuzz")
+    elif i % 3 == 0:
+        print(f"{i}: Fizz")
+    elif i % 5 == 0:
+        print(f"{i}: Buzz")
