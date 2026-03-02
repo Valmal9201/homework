e489ea7650f86de4d14832308e2071ba6644c859
Valmal9201 <zeng.cynthia236@gmail.com>
2026-03-02 09:52:24 -0500

    Prefect Numbers Sum <= 10 000 Problem Solution

--- lesson20/lesson20.py
+++ lesson20/lesson20.py
@@ -1,1 +1,20 @@
 # Lesson 20
+num = range(1, 10000)
+totalSum = 0
+total = 0
+for i in num:
+    divisor = 1
+    quotient = 0
+    while divisor < i:
+        if i % divisor == 0:
+            quotient == i // divisor
+            if quotient == divisor:
+                total += divisor
+            else:
+                total = total + quotient + divisor
+        divisor += 1
+    if total == i:
+        totalSum += total
+    total = 0
+    i += 1
+print(totalSum)
